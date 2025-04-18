6c864a7cf75b28e4e3160520ae3125db66028916
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-04-18 11:55:19 +0600

    Delete command directory

--- command/help.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'help',
-  aliases: ['commands'],
-  description: 'Displays a list of available commands.',
-  execute: async (message) => {
-    const commands = ['uptime', 'ping', 'info', 'cmdinstall'];
-    message.reply(`Here are the available commands: \n${commands.join(', ')}`);
-  }
-};
